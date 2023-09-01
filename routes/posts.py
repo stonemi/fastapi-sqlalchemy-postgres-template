e4c31808dab26177c1aeba568c30fa8bb30882da
Ban <stonemi81@gmail.com>
2023-09-01 17:45:45 +0900

    add

--- routes/posts.py
+++ routes/posts.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 
 from database.connection import get_db
 from schemas.models import DeletePostResponse, Post, UpdatePost
-from utils.post_crud import (
+from sevice.post_crud import (
     post_create,
     post_delete,
     post_get_one,
